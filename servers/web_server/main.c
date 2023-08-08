11f7e7cb571f29d0a2f8bfeafdd4409f9b0a65d1
keisuke713 <nebakei.tkb713@gmail.com>
2023-08-08 21:25:49 +0900

    impl function that extract file path from http request

--- servers/web_server/main.c
+++ servers/web_server/main.c
@@ -1,8 +1,11 @@
 #include <libs/common/print.h>
+#include <libs/common/string.h>
 #include <libs/user/ipc.h>
 #include <libs/user/syscall.h>
+#include <libs/user/malloc.h>
 
 static task_t tcpip_server;
+static char request[512];
 
 // TCPコネクションを開く(パッシブオープン)
 // 合っているかは知らん
@@ -21,6 +24,27 @@ static void init(void) {
     INFO("fd: %d is created", sock);
 }
 
+// スペースに挟まれている文字列をファイルパスとする
+// リクエストは下記のようになっているため
+// GET /path/index.html HTTP1.1
+static char* extract_path(uint8_t *buf) {
+    int start;
+    start = 0;
+    while (*(buf + start++) != 32) {}
+    INFO("message start: %d", *(buf + start));
+
+    int end;
+    end = start;
+    while (*(buf + end++) != 32) {}
+    INFO("message end: %d", *(buf + end));
+
+    int len;
+    len = end - start;
+    char *path = malloc(len);
+    memcpy(path, (void *)(buf + start), len);
+    return path;
+}
+
 void main(void) {
     // 初期化
     init();
@@ -31,21 +55,24 @@ void main(void) {
 
     // メインループ
     while (true) {
-        // struct message m;
-        // ASSERT_OK(ipc_recv(IPC_ANY, &m));
-        // switch (m.type) {
-        //     case PING_MSG: {
-        //         DBG("received ping message from #%d (value=%d)", m.src,
-        //             m.ping.value);
+        struct message m;
+        ASSERT_OK(ipc_recv(IPC_ANY, &m));
 
-        //         m.type = PING_REPLY_MSG;
-        //         m.ping_reply.value = 42;
-        //         ipc_reply(m.src, &m);
-        //         break;
-        //     }
-        //     default:
-        //         WARN("unhandled message: %s (%x)", msgtype2str(m.type), m.type);
-        //         break;
-        // }
+        switch (m.type) {
+            case TCPIP_DATA_MSG: {
+                m.type = TCPIP_READ_MSG;
+                m.tcpip_read.sock = m.tcpip_data.sock;
+                error_t err = ipc_call(tcpip_server, &m);
+
+                ASSERT_OK(err);
+
+                // リクエストを受け取ったらパスを抽出
+                char* path = extract_path((uint8_t *)m.tcpip_read_reply.data);
+                INFO("after start: %s", path);
+
+                // TODO ファイルシステムに該当のファイルを問い合わせて
+                // それをレスポンスと共に返す
+            }
+        }
     }
 }
